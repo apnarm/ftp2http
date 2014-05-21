9bf6969b172692f64d2f22a35b7ce65e433bebbd
Raymond Butcher <randomy@gmail.com>
2014-05-21 01:35:44 +0100

    Deleting password hash code that is no longer used

--- ftp2http/ftp2http.py
+++ ftp2http/ftp2http.py
@@ -14,7 +14,6 @@ be determined. This means that the HTTP request only starts when the file is
 import base64
 import bcrypt
 import errno
-import hashlib
 import os
 import socket
 import sys
@@ -320,15 +319,6 @@ class TLS_PostDTPHandler(PostDTPHandlerMixin, TLS_DTPHandler):
 
 
 class AccountAuthorizer(DummyAuthorizer):
-
-    hash_funcs = {
-        'md5': hashlib.md5,
-        'sha1': hashlib.sha1,
-        'sha224': hashlib.sha224,
-        'sha256': hashlib.sha256,
-        'sha384': hashlib.sha384,
-        'sha512': hashlib.sha512,
-    }
 
     def __init__(self, accounts, http_basic_auth=False, backends=()):
 
